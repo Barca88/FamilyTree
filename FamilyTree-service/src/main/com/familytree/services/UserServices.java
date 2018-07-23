0669ed518e172b6f8696b970f2a4d8e1b2c6ec97
Goncalves <m.goncalves@accenture.com>
2018-07-23 14:34:39 +0100

    Hello world! :speedboat:

--- FamilyTree-service/src/main/com/familytree/services/UserServices.java
+++ FamilyTree-service/src/main/com/familytree/services/UserServices.java
@@ -1,5 +1,18 @@
 package main.com.familytree.services;
 
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/user")
 public class UserServices {
 
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello(){
+		return "Hello World!";
+	}
+	
 }
